9147b25ce6bc86f1995d99387cca492e11cd4197
Jasurbek <jasurisoqov685@gmail.com>
2023-01-09 22:28:37 +0500

    Folder and file structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// Styles
+import "./Assets/Styles/fonts.scss";
+import "./Assets/Styles/index.scss";
+
+const App = () => {
+  return <div>App</div>;
+};
 
 export default App;
