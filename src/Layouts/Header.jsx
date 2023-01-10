922bb2792c97532d24f59d58681ea4d7b911258d
Jasurbek <jasurisoqov685@gmail.com>
2023-01-11 00:33:10 +0500

    Header section finished

--- src/Layouts/Header.jsx
+++ src/Layouts/Header.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
+import arrowBottom from "../Assets/images/icons/arrow-bottom.png";
+import profile from "../Assets/images/Avatar.png";
+import notification from "../Assets/images/icons/notification.png";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <img src={notification} alt="notification ison" />
+      <img src={profile} alt="profile image" />
+      <p>Иван Иванович</p>
+      <img src={arrowBottom} alt="arrow bottom" />
+    </div>
+  );
 };
 
 export default Header;
